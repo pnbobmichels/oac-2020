177f43a4877a3ee4b7960dc09de82ca5f2a7cbc2
Bob <michelbob@hotmail.com>
2020-12-02 09:26:41 +0100

    Day 1

---
+++ day1/solution.pl
@@ -0,0 +1,11 @@
+get_equals_2(Y1, X1, X2, Result):-
+    Y1 is X1 + X2,
+    Result is X1 * X2.
+
+% ? - nmbr(X),nmbr(Y), nmbr(Z), get_equals_2(2020,X,Y,Result)
+
+get_equals_3(Y1, X1, X2, X3, Result):-
+    Y1 is X1 + X2+ X3,
+    Result is X1 * X2 * X3.
+
+% ? - nmbr(X),nmbr(Y), nmbr(Z), get_equals_3(2020,X,Y,Z,Result).
