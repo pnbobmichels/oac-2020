2013384c69acc4b8fe9c441208d53498a592d525
Bob <michelbob@hotmail.com>
2020-12-02 09:29:34 +0100

    Small fix in execution formula comment

--- day1/solution.pl
+++ day1/solution.pl
@@ -2,7 +2,7 @@ get_equals_2(Y1, X1, X2, Result):-
     Y1 is X1 + X2,
     Result is X1 * X2.
 
-% ? - nmbr(X),nmbr(Y), nmbr(Z), get_equals_2(2020,X,Y,Result)
+% ? - nmbr(X),nmbr(Y), get_equals_2(2020,X,Y,Result)
 
 get_equals_3(Y1, X1, X2, X3, Result):-
     Y1 is X1 + X2+ X3,
